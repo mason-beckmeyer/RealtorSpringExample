e4987a87723d7705581512f4e1842b8aae0a9bed
MasonB <mbbeckmeyer9@gmail.com>
2025-11-22 15:21:59 -0500

    First commit for repo

---
+++ demo/src/main/java/com/example/demo/RealtorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtorDemoApplication.class, args);
+	}
+
+}
